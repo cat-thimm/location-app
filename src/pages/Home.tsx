04e103e5c3c2c14b3079d2b4c9134023eb308d73
Caterina <119865119+cat-thimm@users.noreply.github.com>
2024-10-07 11:05:14 +0900

    :bug: update dependencies and update mapbox hook

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,15 +5,10 @@ import './Home.css';
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Location App</IonTitle>
-        </IonToolbar>
-      </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            {/*<IonTitle size="large">Blank</IonTitle>*/}
           </IonToolbar>
         </IonHeader>
         <MapContainer />
